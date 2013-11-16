f9f961fae7c07a252dca1d93e38873d5027ce228
Diego Arias <diagom27@gmail.com>
2013-11-16 16:34:17 -0300

    Adding slate bootstrap, moving navbar to the shared page, and new aobut page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinterest2::Application.config.secret_key_base = 'cc3726a8ac6d88b6583ae877148831bb383c728902f8e0b7182305ea9351250ea4c1608109c5d5da24c78713a5e2cfb3ed24031696bcd2bc4519ded65428e600'
